93b46091fd91f1e57a6e812644a50d7241a2c854
Swati Sinha <swati17.sinha@gmail.com>
2020-05-18 10:09:10 +0530

    OrdersAPI source code added

---
+++ OrdersAPI/Controllers/OrdersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OrdersAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrdersAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+
+        private readonly ILogger<OrdersController> _logger;
+        private readonly List<Order> orders = new List<Order>()
+        {
+            new Order(){OrderId=1, OrderAmount=250, OrderDate= new DateTime(2020,04,14), UserId=1},
+            new Order(){OrderId=2, OrderAmount=450, OrderDate= new DateTime(2020,04,15), UserId=1},
+            new Order(){OrderId=3, OrderAmount=250, OrderDate= new DateTime(2020,04,14), UserId=2},
+            new Order(){OrderId=4, OrderAmount=450, OrderDate= new DateTime(2020,04,15), UserId=2}
+        };
+        public OrdersController(ILogger<OrdersController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public ActionResult<List<Order>> Get()
+        {
+            return orders.ToList();
+        }
+        [HttpGet("{id}")]
+        public ActionResult<List<Order>> Get(int id)
+        {
+            return orders.Where(o => o.UserId.Equals(id)).ToList();
+        }
+    }
+}
---
+++ OrdersAPI/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrdersAPI.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public double OrderAmount { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int UserId { get; set; }
+    }
+}
